4a462a68b010b507f7f6e571e04d1b4402e9d8e7
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-31 22:37:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@ def position_taken?(board, index)
   if (board[index] == "") || (board[index] == "") || (board[index] == nil)
     return false
   else
-    return true  #board[index] is not "" or "" or nil because there's an "X" or "O"
+    return true  #board[index] is not " " or "" or nil because there's an "X" or "O"
   end 
 end
